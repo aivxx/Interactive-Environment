50cb66e148be43f211a427e3ba4a4a84b7816f32
aivxx <90653195+aivxx@users.noreply.github.com>
2021-11-18 19:58:09 -0500

    Create FP movements
    Created input control system and actions for mouse and keyboard. Created script for FPCamera

---
+++ Assets/Input/FPMovement.cs
@@ -0,0 +1,286 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Input/FPMovement.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @FPMovement : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @FPMovement()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""FPMovement"",
+    ""maps"": [
+        {
+            ""name"": ""Controls"",
+            ""id"": ""3254637e-92b4-420a-8425-56c2b17a7c95"",
+            ""actions"": [
+                {
+                    ""name"": ""CameraLook"",
+                    ""type"": ""Value"",
+                    ""id"": ""dc441eff-87d8-4cbd-889a-554e5c1de0a4"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""PlayerMove"",
+                    ""type"": ""Value"",
+                    ""id"": ""611aae9b-ac06-4c41-ad1f-0ec3879fdb25"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""8c3aec48-c6cb-496c-b209-8495a8119869"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and Mouse"",
+                    ""action"": ""CameraLook"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""1c53291c-3fbc-4968-af0f-d3bcbf8f8325"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""PlayerMove"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""a19434c8-1e32-4b55-ab92-7deb0c0a5644"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and Mouse"",
+                    ""action"": ""PlayerMove"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""a9a61dab-c1e1-4311-b418-e5f7de05bc4d"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and Mouse"",
+                    ""action"": ""PlayerMove"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""52176251-a3a1-4df0-8d47-e6a793aef94d"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and Mouse"",
+                    ""action"": ""PlayerMove"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""b8af4229-d493-420c-9c27-5a763b00cced"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and Mouse"",
+                    ""action"": ""PlayerMove"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Arrows"",
+                    ""id"": ""fdd28394-55a4-44c4-b1eb-64be4670d6b5"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""PlayerMove"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""cf0011c5-1de0-4fc8-bc9d-07006998ed72"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and Mouse"",
+                    ""action"": ""PlayerMove"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""628f6ee4-f03a-427e-8ea1-fe2618b1cbca"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and Mouse"",
+                    ""action"": ""PlayerMove"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""986610fe-6c09-41e7-8e16-a3ed3d5a9cf7"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and Mouse"",
+                    ""action"": ""PlayerMove"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""96d3f412-5f21-4231-940f-91d7e30c51ac"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and Mouse"",
+                    ""action"": ""PlayerMove"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Keyboard and Mouse"",
+            ""bindingGroup"": ""Keyboard and Mouse"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                },
+                {
+                    ""devicePath"": ""<Mouse>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // Controls
+        m_Controls = asset.FindActionMap("Controls", throwIfNotFound: true);
+        m_Controls_CameraLook = m_Controls.FindAction("CameraLook", throwIfNotFound: true);
+        m_Controls_PlayerMove = m_Controls.FindAction("PlayerMove", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Controls
+    private readonly InputActionMap m_Controls;
+    private IControlsActions m_ControlsActionsCallbackInterface;
+    private readonly InputAction m_Controls_CameraLook;
+    private readonly InputAction m_Controls_PlayerMove;
+    public struct ControlsActions
+    {
+        private @FPMovement m_Wrapper;
+        public ControlsActions(@FPMovement wrapper) { m_Wrapper = wrapper; }
+        public InputAction @CameraLook => m_Wrapper.m_Controls_CameraLook;
+        public InputAction @PlayerMove => m_Wrapper.m_Controls_PlayerMove;
+        public InputActionMap Get() { return m_Wrapper.m_Controls; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(ControlsActions set) { return set.Get(); }
+        public void SetCallbacks(IControlsActions instance)
+        {
+            if (m_Wrapper.m_ControlsActionsCallbackInterface != null)
+            {
+                @CameraLook.started -= m_Wrapper.m_ControlsActionsCallbackInterface.OnCameraLook;
+                @CameraLook.performed -= m_Wrapper.m_ControlsActionsCallbackInterface.OnCameraLook;
+                @CameraLook.canceled -= m_Wrapper.m_ControlsActionsCallbackInterface.OnCameraLook;
+                @PlayerMove.started -= m_Wrapper.m_ControlsActionsCallbackInterface.OnPlayerMove;
+                @PlayerMove.performed -= m_Wrapper.m_ControlsActionsCallbackInterface.OnPlayerMove;
+                @PlayerMove.canceled -= m_Wrapper.m_ControlsActionsCallbackInterface.OnPlayerMove;
+            }
+            m_Wrapper.m_ControlsActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @CameraLook.started += instance.OnCameraLook;
+                @CameraLook.performed += instance.OnCameraLook;
+                @CameraLook.canceled += instance.OnCameraLook;
+                @PlayerMove.started += instance.OnPlayerMove;
+                @PlayerMove.performed += instance.OnPlayerMove;
+                @PlayerMove.canceled += instance.OnPlayerMove;
+            }
+        }
+    }
+    public ControlsActions @Controls => new ControlsActions(this);
+    private int m_KeyboardandMouseSchemeIndex = -1;
+    public InputControlScheme KeyboardandMouseScheme
+    {
+        get
+        {
+            if (m_KeyboardandMouseSchemeIndex == -1) m_KeyboardandMouseSchemeIndex = asset.FindControlSchemeIndex("Keyboard and Mouse");
+            return asset.controlSchemes[m_KeyboardandMouseSchemeIndex];
+        }
+    }
+    public interface IControlsActions
+    {
+        void OnCameraLook(InputAction.CallbackContext context);
+        void OnPlayerMove(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/FPCamera.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FPCamera : MonoBehaviour
+{
+    // Camera attached to player
+    public Camera playerCamera;
+
+    //Container variables for the mouse delta values each frame
+    public float deltaX;
+    public float deltaY;
+
+    //Container variables for the player's rotation around the X and Y axis
+    public float xRot; //rotation x in degrees
+    public float yRot; //rotation y in degrees
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCamera = Camera.main; // set player camera
+        Cursor.visible = false; //hide cursor
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Keep track of the player's x and y rotation
+        yRot += deltaX;
+        xRot -= deltaY;
+
+        // Keep player's x rotation clamped
+        xRot = Mathf.Clamp(xRot, -90f, 90f);
+
+        // rotate camera around x axis
+        playerCamera.transform.localRotation = Quaternion.Euler(xRot, 0, 0);
+        transform.rotation = Quaternion.Euler(0, yRot, 0);
+    }
+
+    // OnCameraLook event handler
+    public void OnCameraLook(InputValue value)
+    {
+        // Reading mouse delta's as a vector 2 (delta x is x-component and delta y is y-component)
+        Vector2 inputVector = value.Get<Vector2>();
+        deltaX = inputVector.x;
+        deltaY = inputVector.y;
+    }
+
+}
