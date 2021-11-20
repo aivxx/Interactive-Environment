09412256cb23d7b108f64dd3b029778754d99493
aivxx <90653195+aivxx@users.noreply.github.com>
2021-11-20 12:13:03 -0500

    Create clues
    also created script to show tree upon collection of clue 15

---
+++ Assets/Scripts/Clues/ClueUI.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+
+public class ClueUI : MonoBehaviour
+{
+    
+    public GameObject uiObject;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            uiObject.SetActive(true);
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            uiObject.SetActive(false);
+        }
+    }
+
+}
