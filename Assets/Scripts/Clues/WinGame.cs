140cbd88c2321da0d632d0d7b969974d1a793f50
aivxx <90653195+aivxx@users.noreply.github.com>
2021-11-20 12:33:51 -0500

    Create game menus and scripts

--- Assets/Scripts/Clues/WinGame.cs
+++ Assets/Scripts/Clues/WinGame.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class WinGame : MonoBehaviour
 {
+    public GameObject uiMenu;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        uiMenu.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.CompareTag("Player"))
+        {
+            uiMenu.SetActive(true);
+        }
     }
+
+
 }
